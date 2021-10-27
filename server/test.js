428622df54ceedec707b16e8a2105dd5c64837f9
Yicong Li <leo727268082@gmail.com>
2021-10-28 02:31:08 +1000

    modify back end router, and add persistence

--- server/test.js
+++ server/test.js
@@ -6,9 +6,17 @@ const axios = require('axios').default;
 const { response } = require('express');
 require('dotenv').config();
 const AWS = require('aws-sdk');
+const natrual = require('natural')
+const aposToLexForm = require('apos-to-lex-form');
+const SW = require('stopword')
+const Analyzer = require('natural').SentimentAnalyzer;
+const stemmer = require('natural').PorterStemmer;
+
+
+
+
 const app = express()
 const port = 3000
-
 
 
 
@@ -19,9 +27,6 @@ const client = redis.createClient();
 client.on('error', (err) => {
   console.log("Error " + err);
 });
-
-
-
 
 
 // Cloud Services Set-up
@@ -36,18 +41,10 @@ bucketPromise.then(function(data) {
   });
 
   //store data in Redis --- twitter
-  function StoreTwitterData(query, responseJSON){
+  function StoreTwitterData(searchQuery, tweetData){
 
-    return client.setex(`twitter:${query}`, 3600, JSON.stringify({ source: 'Redis Cache', ...responseJSON, }));
+    return client.setex(`twitter:${searchQuery}`, 3600, JSON.stringify({ source: 'Redis Cache',...tweetData}));
   }
-
-    //store data in Redis --- country
-    function StoreTwitterData(query, responseJSON){
-
-      return client.setex(`restcountries API: ${query}`, 3600, JSON.stringify({ source: 'Redis Cache', ...responseJSON, }));
-    }
-
-
 
 
 
@@ -65,90 +62,76 @@ const bearToken = `AAAAAAAAAAAAAAAAAAAAABgCUwEAAAAAulp6nbJYV0eZsgfF%2F35nkcbcfxg
 
 
 
-
-app.get("/api/countries", (req, res) => {
-
-  let country = `https://restcountries.com/v2/all/api/countries`
-
-
-  const s3Key = `restcountries API: ${country}`; // Check S3
-  const params = { Bucket: bucketName, Key: s3Key};
-  // Try fetching the result from Redis first in case we have it cached
-
-  const url = "https://restcountries.com/v2/all"
-
-  return client.get(`restcountries API: ${country}`, (err, result) =>{
-
-    if(result){
-
-      const resultJSON = JSON.parse(result);
-      // console.log(resultJSON)
-      return res.status(200).json(resultJSON);
-
-    }else{
-      
-      return new AWS.S3({apiVersion: '2006-03-01'}).getObject(params, (err, result2) => { 
-
-        if(result2){
-
-          // Serve from S3
-          console.log(result2);
-          const resultJSON = JSON.parse(result2.Body);
-          StoreData(country, resultJSON) 
-          return res.status(200).json(resultJSON);
-
-        }
-        else{
-  
-          axios.get(url)
-          .then(response => {
-            const responseJSON = response.data;
-            // Save the Wikipedia API response in Redis store
-            client.setex(`restcountries API: ${country}`, 3600, JSON.stringify({ source: 'Redis Cache',...responseJSON, }));
-
-            const body = JSON.stringify({ source: 'S3 Bucket', ...responseJSON}); 
-            const objectParams = {Bucket: bucketName, Key: s3Key, Body: body}; 
-            const uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise(); 
-            return res.status(200).json({ source: 'restcountries API', ...responseJSON, });
-          })
-          .catch(e => {
-            console.log(e)
-          })
-        }
-      })
-    }
-  }) 
-})
-  
-
-
-
-
 //search recent tweet
 app.get('/api/tweet/search', (req, res) => {
 
-  let query = req.query
-  // console.log(query.q)
+  // let query = req.query
+  // // console.log(query.q)
 
-  const s3Key = `twitter:${query.q}`; // Check S3
-  const params = { Bucket: bucketName, Key: s3Key};
-  // Try fetching the result from Redis first in case we have it cached
+  // // Try fetching the result from Redis first in case we have it cached
+
+  // var searchInRecentTweets = {
+  //   method: "GET",
+  //   url: `${twitterAPI}/search/tweets.json?q=${query.q}&count=100&lang=en`,
+  //   headers: {
+  //     'Authorization': `Bearer ${bearToken}`
+  //   }
+  // }
+
+
+  let searchQuery = req.query.q
+  let hashtag = null
+  let type = null
+
+  if (req.query.hasOwnProperty('h')){
+    hashtag = req.query.h
+  }
+  if (req.query.hasOwnProperty('t')){
+    type = req.query.t
+  }
+
+  let url = `${twitterAPI}/search/tweets.json?q=${searchQuery == null ? "" : searchQuery}${hashtag == null ? "" : "%23" + hashtag}&count=100&lang=en${type == null ? "" : "&result_type=" + type}&tweet_mode=extended`
 
   var searchInRecentTweets = {
     method: "GET",
-    url: `${twitterAPI}/search/tweets.json?q=${query.q}&count=100&lang=en`,
+    url: url,
     headers: {
       'Authorization': `Bearer ${bearToken}`
     }
   }
 
-  return client.get(`twitter:${query.q}`, (err, result) =>{
+
+  const s3Key = `twitter:${searchQuery}`; // Check S3
+  const params = { Bucket: bucketName, Key: s3Key};
+
+
+
+
+  return client.get(`twitter:${searchQuery}`, (err, result) =>{
     
     //check redis first
     if(result){
       const resultJSON = JSON.parse(result);
+      console.log("redis return")
       // console.log(resultJSON)
-      return res.status(200).json(resultJSON);
+      // return res.status(200).json(resultJSON);
+
+        //analysis
+        const {WordTokenizer} = natrual
+        const tokenizer = new WordTokenizer()
+        const analyzer = new Analyzer("English", stemmer, "afinn");
+        const sentimentAnalysisResults = resultJSON.statuses.map(tweet => analyzer.getSentiment(SW.removeStopwords(tokenizer.tokenize(aposToLexForm(tweet.full_text).toLowerCase().replace(/[^a-zA-Z\s]+/g, '')))))
+        const filteredData = resultJSON.statuses.map(tweet => {
+          let tweetObj = {
+              time: tweet.created_at,
+              name: tweet.user.name,
+              screen_name: tweet.user.screen_name,
+              text: tweet.full_text
+          }
+          return tweetObj
+        })
+      // return res.status(200).json(resultJSON);
+      res.status(200).json({tweetData: filteredData, sentimentAnalysis: sentimentAnalysisResults});
     }
     else{
 
@@ -157,24 +140,60 @@ app.get('/api/tweet/search', (req, res) => {
 
         if (result2) {
             // Serve from S3
-            console.log(result2);
+            console.log("s3 return")
             const resultJSON = JSON.parse(result2.Body);
-            StoreTwitterData(query.q, resultJSON) 
-            return res.status(200).json(resultJSON);
+            StoreTwitterData(searchQuery, resultJSON) 
+              //analysis
+              const {WordTokenizer} = natrual
+              const tokenizer = new WordTokenizer()
+              const analyzer = new Analyzer("English", stemmer, "afinn");
+              const sentimentAnalysisResults = resultJSON.statuses.map(tweet => analyzer.getSentiment(SW.removeStopwords(tokenizer.tokenize(aposToLexForm(tweet.full_text).toLowerCase().replace(/[^a-zA-Z\s]+/g, '')))))
+              const filteredData = resultJSON.statuses.map(tweet => {
+                let tweetObj = {
+                    time: tweet.created_at,
+                    name: tweet.user.name,
+                    screen_name: tweet.user.screen_name,
+                    text: tweet.full_text
+                }
+                return tweetObj
+              })
+            // return res.status(200).json(resultJSON);
+            res.status(200).json({tweetData: filteredData, sentimentAnalysis: sentimentAnalysisResults});
         }
 
         //fetch data from twitter api 
         else{
           axios.request(searchInRecentTweets)
+          
           .then(response => {
-            const responseJSON = response.data;
-            // Save the Wikipedia API response in Redis store
-            client.setex(`twitter:${query.q}`, 3600, JSON.stringify({ source: 'Redis Cache',...responseJSON, }));
+              //res.json(response.data) raw data
+            const tweetData = response.data
 
-            const body = JSON.stringify({ source: 'S3 Bucket', ...responseJSON}); 
+            // Save the Wikipedia API response in Redis store
+            client.setex(`twitter:${searchQuery}`, 3600, JSON.stringify({ source: 'Redis Cache',...tweetData}));
+            //Bucket s3
+            const body = JSON.stringify({ source: 'S3 Bucket', ...tweetData}); 
             const objectParams = {Bucket: bucketName, Key: s3Key, Body: body}; 
             const uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise(); 
-            return res.status(200).json({ source: 'Twitter API', ...responseJSON, });
+
+              //analysis
+              const {WordTokenizer} = natrual
+              const tokenizer = new WordTokenizer()
+              const analyzer = new Analyzer("English", stemmer, "afinn");
+              const sentimentAnalysisResults = tweetData.statuses.map(tweet => analyzer.getSentiment(SW.removeStopwords(tokenizer.tokenize(aposToLexForm(tweet.full_text).toLowerCase().replace(/[^a-zA-Z\s]+/g, '')))))
+              const filteredData = tweetData.statuses.map(tweet => {
+                let tweetObj = {
+                    time: tweet.created_at,
+                    name: tweet.user.name,
+                    screen_name: tweet.user.screen_name,
+                    text: tweet.full_text
+                }
+                return tweetObj
+              })
+
+              console.log("api return")
+              // return res.status(200).json({ source: 'Twitter API', ...tweetData, });
+              res.status(200).json({tweetData: filteredData, sentimentAnalysis: sentimentAnalysisResults});
             }
           )
           .catch(error =>
