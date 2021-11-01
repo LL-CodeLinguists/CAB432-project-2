0a0474985197d8338b035c496b3e1fa43203b63a
Yicong Li <leo727268082@gmail.com>
2021-11-01 12:27:54 +1000

    update the build folder

---
+++ client/build/precache-manifest.0191219d4430d0e709c143aeaa62fdd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a3af26637fb7ed303a6c3e98bcdab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36cabd0316d0d0992546",
+    "url": "/static/css/2.709e72d0.chunk.css"
+  },
+  {
+    "revision": "e69c56e0cc6ac6557fb4",
+    "url": "/static/css/main.39655b32.chunk.css"
+  },
+  {
+    "revision": "36cabd0316d0d0992546",
+    "url": "/static/js/2.0143b7a3.chunk.js"
+  },
+  {
+    "revision": "213dd73e6af15cd012a02fe05a68251c",
+    "url": "/static/js/2.0143b7a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69c56e0cc6ac6557fb4",
+    "url": "/static/js/main.3a0d25cc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
